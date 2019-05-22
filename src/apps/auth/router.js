57ced97eb1ccdd6a7dc6beacb49bc0229b43c3e7
AndreyKomin <andrey.s.komin@gmail.com>
2019-05-22 16:11:51 +0300

    Implement Joi validator

--- src/apps/auth/router.js
+++ src/apps/auth/router.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import { wrapAsync } from 'src/util';
+import expressJoi from 'express-joi-validator';
+import Joi from 'joi';
 import { instagramAuth } from './services';
 
 const app = express();
 
-app.post('/instagram', wrapAsync(async (req, res) => {
+const bodySchema = {
+  body: {
+    clientId: Joi.string().required(),
+    redirectUri: Joi.string().required(),
+    code: Joi.string().required(),
+  },
+};
+
+app.post('/instagram', expressJoi(bodySchema), wrapAsync(async (req, res) => {
   const { code, redirectUri } = req.body;
 
   const response = await instagramAuth(code, redirectUri);
